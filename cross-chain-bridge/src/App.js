c7695cf12f08018170af2117c0b2a182fdc9130f
Prasoon1308 <prasoon1308@gmail.com>
2024-02-20 23:56:40 +0530

    withdraw added (L2-->L1)

--- cross-chain-bridge/src/App.js
+++ cross-chain-bridge/src/App.js
@@ -1,28 +1,57 @@
-import React, { useState } from 'react';
-import ContractService from './contractService';
+import React, { useState } from "react";
+import ContractService from "./contractService";
 
 function App() {
-  const [tokenAddress, setTokenAddress] = useState('');
-  const [amount, setAmount] = useState('');
-  const [data, setData] = useState('');
-  const [signature, setSignature] = useState('');
+  const [rootTokenAddress, setRootTokenAddress] = useState("");
+  const [childTokenAddress, setChildTokenAddress] = useState("");
+  const [rootAmount, setRootAmount] = useState("");
+  const [childAmount, setChildAmount] = useState("");
+  const [L2Data, setL2Data] = useState("");
+  const [L1Data, setL1Data] = useState("");
+  const [L2Signature, setL2Signature] = useState("");
+  const [L1Signature, setL1Signature] = useState("");
 
   const handleDeposit = async () => {
     try {
-      await ContractService.depositOnL1(tokenAddress, amount);
-      console.log('Deposit on L1 successful!');
+      const {data, signature} = await ContractService.depositOnL1(rootTokenAddress, rootAmount);
+      console.log("data: ", data);
+      console.log("signature: ", signature);
+      setL2Data(data);
+      setL2Signature(signature);
+      console.log("Deposit on L1 successful!");
     } catch (error) {
-      console.error('Error depositing on L1:', error.message);
+      console.error("Error depositing on L1:", error.message);
     }
   };
-
   const handleMint = async () => {
     // Assuming the user copies data and signature from the Deposit on L1 section
     try {
-      await ContractService.mintOnL2(data, signature);
-      console.log('Mint on L2 successful!');
+      await ContractService.mintOnL2(L2Data, L2Signature);
+      console.log("Mint on L2 successful!");
     } catch (error) {
-      console.error('Error minting on L2:', error.message);
+      console.error("Error minting on L2:", error.message);
+    }
+  };
+
+  const handleBurn = async () => {
+    try {
+      const {data, signature} = await ContractService.burnOnL2(childTokenAddress, childAmount);
+      console.log("data: ", data);
+      console.log("signature: ", signature);
+      setL1Data(data);
+      setL1Signature(signature);
+      console.log("Deposit on L1 successful!");
+    } catch (error) {
+      console.error("Error burning on L2:", error.message);
+    }
+  };
+  const handleWithdraw = async () => {
+    // Assuming the user copies data and signature from the Deposit on L1 section
+    try {
+      await ContractService.withdrawOnL1(L1Data, L1Signature);
+      console.log("Mint on L2 successful!");
+    } catch (error) {
+      console.error("Error withdrawing on L1:", error.message);
     }
   };
 
@@ -34,16 +63,30 @@ function App() {
       <div>
         <h2>Deposit on L1</h2>
         <label>
-          Token Address:
-          <input type="text" value={tokenAddress} onChange={(e) => setTokenAddress(e.target.value)} />
+          Root Token Address:
+          <input
+            type="text"
+            value={rootTokenAddress}
+            onChange={(e) => setRootTokenAddress(e.target.value)}
+          />
         </label>
         <br />
         <label>
-          Amount:
-          <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+         Root Token Amount:
+          <input
+            type="text"
+            value={rootAmount}
+            onChange={(e) => setRootAmount(e.target.value)}
+          />
         </label>
         <br />
         <button onClick={handleDeposit}>Deposit</button>
+      </div>
+
+      {/* Display Data and Signature */}
+      <div>
+        <p>Data: {L2Data}</p>
+        <p>Signature: {L2Signature}</p>
       </div>
 
       {/* Mint on L2 Section */}
@@ -51,15 +94,77 @@ function App() {
         <h2>Mint on L2</h2>
         <label>
           Data:
-          <input type="text" value={data} onChange={(e) => setData(e.target.value)} />
+          <input
+            type="text"
+            value={L2Data}
+            onChange={(e) => setL2Data(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Signature:
-          <input type="text" value={signature} onChange={(e) => setSignature(e.target.value)} />
+          <input
+            type="text"
+            value={L2Signature}
+            onChange={(e) => setL2Signature(e.target.value)}
+          />
         </label>
         <br />
         <button onClick={handleMint}>Mint</button>
+      </div>
+
+      {/* Burn on L2 Section */}
+      <div>
+        <h2>Burn on L2</h2>
+        <label>
+          Child Token Address:
+          <input
+            type="text"
+            value={childTokenAddress}
+            onChange={(e) => setChildTokenAddress(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+         Child Token Amount:
+          <input
+            type="text"
+            value={childAmount}
+            onChange={(e) => setChildAmount(e.target.value)}
+          />
+        </label>
+        <br />
+        <button onClick={handleBurn}>Burn</button>
+      </div>
+
+      {/* Display Data and Signature */}
+      <div>
+        <p>Data: {L1Data}</p>
+        <p>Signature: {L1Signature}</p>
+      </div>
+
+      {/* Withdraw on L1 Section */}
+      <div>
+        <h2>Withdraw on L1</h2>
+        <label>
+          Data:
+          <input
+            type="text"
+            value={L1Data}
+            onChange={(e) => setL1Data(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Signature:
+          <input
+            type="text"
+            value={L1Signature}
+            onChange={(e) => setL1Signature(e.target.value)}
+          />
+        </label>
+        <br />
+        <button onClick={handleWithdraw}>Withdraw</button>
       </div>
     </div>
   );
--- cross-chain-bridge/src/contractService.js
+++ cross-chain-bridge/src/contractService.js
@@ -13,31 +13,20 @@ const bridgeDeployerV3Abi = require("./ABI/BridgeDeployerV3.json");
 const tokenL1Abi = require("./ABI/TokenL1.json");
 const tokenL2Abi = require("./ABI/TokenL2.json");
 
-const bridgeFactoryV3Address = config.BridgeFactoryV3.goerli;
-const bridgeDeployerV3Address = config.BridgeDeployerV3.mumbai;
+const bridgeFactoryV3Address = config.BridgeFactoryLast.goerli;
+const bridgeDeployerV3Address = config.BridgeDeployerLast.mumbai;
+
+let signer = null;
+let provider;
+if (window.ethereum == null) {
+  console.log("MetaMask not installed; using read-only defaults");
+  provider = ethers.getDefaultProvider();
+} else {
+  provider = new ethers.BrowserProvider(window.ethereum);
+  signer = await provider.getSigner();
+}
 
 const depositOnL1 = async function (tokenAddress, amount) {
-  let signer = null;
-
-  let provider;
-  if (window.ethereum == null) {
-    // If MetaMask is not installed, we use the default provider,
-    // which is backed by a variety of third-party services (such
-    // as INFURA). They do not have private keys installed,
-    // so they only have read-only access
-    console.log("MetaMask not installed; using read-only defaults");
-    provider = ethers.getDefaultProvider();
-  } else {
-    // Connect to the MetaMask EIP-1193 object. This is a standard
-    // protocol that allows Ethers access to make all read-only
-    // requests through MetaMask.
-    provider = new ethers.BrowserProvider(window.ethereum);
-
-    // It also provides an opportunity to request access to write
-    // operations, which will be performed by the private key
-    // that MetaMask manages for the user.
-    signer = await provider.getSigner();
-  }
   const bridgeFactoryV3 = new ethers.Contract(
     bridgeFactoryV3Address,
     bridgeFactoryV3Abi,
@@ -47,54 +36,78 @@ const depositOnL1 = async function (tokenAddress, amount) {
   const transaction = await bridgeFactoryV3.depositOnL1(tokenAddress, amount);
   const receipt = await transaction.wait();
   console.log(receipt);
+  console.log(receipt.blockHash);
+  console.log(signer.address);
 
   let data;
   let signature;
-  bridgeFactoryV3.on(
-    bridgeFactoryV3.filters.DepositLog,
-    async (rootTokenAddress, fromAddress, amount, nonce, event) => {
-      const transactionAmountL1 = ethers.toNumber(amount);
-      const transactionNonceL1 = ethers.toNumber(nonce);
+  //   bridgeFactoryV3.on(
+  //     bridgeFactoryV3.filters.DepositLog,
+  //     async (rootTokenAddress, fromAddress, amount, nonce, event) => {
+  //       const transactionAmountL1 = ethers.toNumber(amount);
+  //       const transactionNonceL1 = ethers.toNumber(nonce);
 
-      data = await generateDataL2(
-        rootTokenAddress,
-        fromAddress,
-        transactionAmountL1,
-        transactionNonceL1
-      );
-      signature = await generateSignatureL2(
-        rootTokenAddress,
-        fromAddress,
-        transactionAmountL1,
-        transactionNonceL1
-      );
-    }
-  );
-  return { data, signature };
+  //       if (fromAddress === signer.address) {
+  //         data = await generateDataL2(
+  //             rootTokenAddress,
+  //             fromAddress,
+  //             transactionAmountL1,
+  //             transactionNonceL1
+  //           );
+  //           signature = await generateSignatureL2(
+  //             rootTokenAddress,
+  //             fromAddress,
+  //             transactionAmountL1,
+  //             transactionNonceL1
+  //           );
+  //           return {data, signature};
+  //       }
+  //     }
+  //   );
+  return new Promise(async (resolve, reject) => {
+    const listener = async (
+      rootTokenAddress,
+      fromAddress,
+      amount,
+      nonce,
+      event
+    ) => {
+      try {
+        const transactionAmountL1 = ethers.toNumber(amount);
+        const transactionNonceL1 = ethers.toNumber(nonce);
+
+        const data = await generateDataL2(
+          rootTokenAddress,
+          fromAddress,
+          transactionAmountL1,
+          transactionNonceL1
+        );
+        const signature = await generateSignatureL2(
+          rootTokenAddress,
+          fromAddress,
+          transactionAmountL1,
+          transactionNonceL1
+        );
+
+        // Remove the listener after resolving the Promise
+        bridgeFactoryV3.off(bridgeFactoryV3.filters.DepositLog, listener);
+
+        resolve({ data, signature });
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    // Set up the event listener
+    bridgeFactoryV3.on(bridgeFactoryV3.filters.DepositLog, listener);
+  });
 };
 
 const mintOnL2 = async (data, signature) => {
-  let signer = null;
-
-  let provider;
-  if (window.ethereum == null) {
-    // If MetaMask is not installed, we use the default provider,
-    // which is backed by a variety of third-party services (such
-    // as INFURA). They do not have private keys installed,
-    // so they only have read-only access
-    console.log("MetaMask not installed; using read-only defaults");
-    provider = ethers.getDefaultProvider();
-  } else {
-    // Connect to the MetaMask EIP-1193 object. This is a standard
-    // protocol that allows Ethers access to make all read-only
-    // requests through MetaMask.
-    provider = new ethers.BrowserProvider(window.ethereum);
-
-    // It also provides an opportunity to request access to write
-    // operations, which will be performed by the private key
-    // that MetaMask manages for the user.
-    signer = await provider.getSigner();
-  }
+//   await provider.request({
+//     method: "wallet_switchEthereumChain",
+//     params: [{ chainId: ethers.utils.hexValue(80001) }],
+//   });
   const bridgeDeployerV3 = new ethers.Contract(
     bridgeDeployerV3Address,
     bridgeDeployerV3Abi,
@@ -104,7 +117,103 @@ const mintOnL2 = async (data, signature) => {
   await bridgeDeployerV3.mintOnL2(data, signature);
 };
 
+const burnOnL2 = async function (childTokenAddress, childAmount) {
+  const bridgeDeployerV3 = new ethers.Contract(
+    bridgeDeployerV3Address,
+    bridgeDeployerV3Abi,
+    signer
+  );
+
+  const transaction = await bridgeDeployerV3.burnOnL2(
+    childTokenAddress,
+    childAmount
+  );
+  const receipt = await transaction.wait();
+  console.log(receipt);
+  console.log(receipt.blockHash);
+  console.log(signer.address);
+
+  let data;
+  let signature;
+  //   bridgeFactoryV3.on(
+  //     bridgeFactoryV3.filters.DepositLog,
+  //     async (rootTokenAddress, fromAddress, amount, nonce, event) => {
+  //       const transactionAmountL1 = ethers.toNumber(amount);
+  //       const transactionNonceL1 = ethers.toNumber(nonce);
+
+  //       if (fromAddress === signer.address) {
+  //         data = await generateDataL2(
+  //             rootTokenAddress,
+  //             fromAddress,
+  //             transactionAmountL1,
+  //             transactionNonceL1
+  //           );
+  //           signature = await generateSignatureL2(
+  //             rootTokenAddress,
+  //             fromAddress,
+  //             transactionAmountL1,
+  //             transactionNonceL1
+  //           );
+  //           return {data, signature};
+  //       }
+  //     }
+  //   );
+  return new Promise(async (resolve, reject) => {
+    const listener = async (
+      childTokenAddress,
+      fromAddress,
+      amount,
+      nonce,
+      event
+    ) => {
+      try {
+        const transactionAmountL2 = ethers.toNumber(amount);
+        const transactionNonceL2 = ethers.toNumber(nonce);
+
+        const data = await generateDataL1(
+          childTokenAddress,
+          fromAddress,
+          transactionAmountL2,
+          transactionNonceL2
+        );
+        const signature = await generateSignatureL1(
+          childTokenAddress,
+          fromAddress,
+          transactionAmountL2,
+          transactionNonceL2
+        );
+
+        // Remove the listener after resolving the Promise
+        bridgeDeployerV3.off(bridgeDeployerV3.filters.BurnLog, listener);
+
+        resolve({ data, signature });
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    // Set up the event listener
+    bridgeDeployerV3.on(bridgeDeployerV3.filters.DepositLog, listener);
+  });
+};
+
+const withdrawOnL1 = async (data, signature) => {
+//   await provider.request({
+//     method: "wallet_switchEthereumChain",
+//     params: [{ chainId: ethers.utils.hexValue(5) }],
+//   });
+  const bridgeFactoryV3 = new ethers.Contract(
+    bridgeFactoryV3Address,
+    bridgeFactoryV3Abi,
+    signer
+  );
+
+  await bridgeFactoryV3.withdrawOnL1(data, signature);
+};
+
 export default {
   depositOnL1,
   mintOnL2,
+  burnOnL2,
+  withdrawOnL1,
 };
